130b6267f370c2d66a11a821a88649fd5591af8f
Adam Chmara <adam.chmara1@gmail.com>
2020-03-16 17:03:20 +0100

    Page build
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

---
+++ build/precache-manifest.bc84af6892cce2191337bd4034f3d4a6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16472914a347d5f6612937c8e0ce12fc",
+    "url": "/frinxio/index.html"
+  },
+  {
+    "revision": "1d4e63ebe6832becf710",
+    "url": "/frinxio/static/css/2.e05f5f1e.chunk.css"
+  },
+  {
+    "revision": "74ed4e36be6936f3329c",
+    "url": "/frinxio/static/css/main.4f6bb571.chunk.css"
+  },
+  {
+    "revision": "1d4e63ebe6832becf710",
+    "url": "/frinxio/static/js/2.3e158d28.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/frinxio/static/js/2.3e158d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ed4e36be6936f3329c",
+    "url": "/frinxio/static/js/main.d4de68bf.chunk.js"
+  },
+  {
+    "revision": "72f233c5ba3349c33bd0",
+    "url": "/frinxio/static/js/runtime-main.a2742fa1.js"
+  },
+  {
+    "revision": "593768c0ecdaab7c2ee17d56f3e6314f",
+    "url": "/frinxio/static/media/about1.593768c0.svg"
+  },
+  {
+    "revision": "b76aabe246beef2a430d69dc26e82839",
+    "url": "/frinxio/static/media/about2.b76aabe2.svg"
+  },
+  {
+    "revision": "1c31de46a4eea78ace6c02bbf5ed28a4",
+    "url": "/frinxio/static/media/about3.1c31de46.svg"
+  },
+  {
+    "revision": "3c77c90fe3a5f91337bd0c90ee6f7392",
+    "url": "/frinxio/static/media/about4.3c77c90f.svg"
+  },
+  {
+    "revision": "bbe446c2392d17edb4fbdc18d7200488",
+    "url": "/frinxio/static/media/ansible.bbe446c2.png"
+  },
+  {
+    "revision": "10eb636698b10e99a671e2811ed83e71",
+    "url": "/frinxio/static/media/banner1.10eb6366.jpg"
+  },
+  {
+    "revision": "6c698b3cefbf9b847818a760e71bc65c",
+    "url": "/frinxio/static/media/bg-arrow.6c698b3c.png"
+  },
+  {
+    "revision": "aa73d45190f0ed729494f1f588c122d9",
+    "url": "/frinxio/static/media/conductor.aa73d451.png"
+  },
+  {
+    "revision": "b84e5da738ac4942200dd4400b3ac378",
+    "url": "/frinxio/static/media/devices.b84e5da7.png"
+  },
+  {
+    "revision": "9f7d39769deb6de202512bf79499bc27",
+    "url": "/frinxio/static/media/elastic.9f7d3976.png"
+  },
+  {
+    "revision": "c222ba9fb8c99c2be51c29f48c0495c3",
+    "url": "/frinxio/static/media/fm-perspective.c222ba9f.png"
+  },
+  {
+    "revision": "e1e614129e3617e9a458040cc40d499b",
+    "url": "/frinxio/static/media/fm-stacked.e1e61412.png"
+  },
+  {
+    "revision": "ded92a644d7b5d005fefb0973a1816f4",
+    "url": "/frinxio/static/media/frinxmachine.ded92a64.png"
+  },
+  {
+    "revision": "76fc9aa252b70c510358bf5b9b4c1abc",
+    "url": "/frinxio/static/media/python.76fc9aa2.png"
+  },
+  {
+    "revision": "518637fc375e4d063c1d1626f9190ff5",
+    "url": "/frinxio/static/media/redis.518637fc.png"
+  },
+  {
+    "revision": "7e8bd58899e85451d1dcb763d6aaa51f",
+    "url": "/frinxio/static/media/terraform.7e8bd588.png"
+  },
+  {
+    "revision": "52801f12612ef461dc41fcc673c6bcce",
+    "url": "/frinxio/static/media/uniconfig.52801f12.png"
+  }
+]);

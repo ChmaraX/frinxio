d1b67cf9132f3b044db086230b52eaff848ebd7d
Adam Chmara <adam.chmara1@gmail.com>
2020-03-30 12:06:30 +0200

    Disable https
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- server.js
+++ server.js
@@ -49,30 +49,36 @@ app.get("/*", function(req, res) {
 });
 
 const port = process.env.NODE_PORT || 3001;
-const host = process.env.NODE_HOST || "0.0.0.0";
+
+app.listen(port, function() {
+  console.log("Server is listening at %s", port);
+  if (process.send) {
+    process.send("online");
+  }
+});
 
 // if certificates folder is empty use HTTP
 // else HTTPS is used (key.pem and cert.pem must be present in folder)
-fs.readdir("./certificates", function(err, files) {
-  if (files.length <= 1) {
-    app.listen(port, host, function() {
-      console.log("Server is listening at http://%s:%s", host, port);
-      if (process.send) {
-        process.send("online");
-      }
-    });
-  } else {
-    https
-      .createServer(
-        {
-          key: fs.readFileSync("./certificates/key.pem"),
-          cert: fs.readFileSync("./certificates/cert.pem"),
-          passphrase: process.env.PASSPHRASE
-        },
-        app
-      )
-      .listen(port, function() {
-        console.log("Server is listening at http://%s:%s", port);
-      });
-  }
-});
+// fs.readdir("./certificates", function(err, files) {
+//   if (files.length <= 1) {
+//     app.listen(port, host, function() {
+//       console.log("Server is listening at http://%s:%s", host, port);
+//       if (process.send) {
+//         process.send("online");
+//       }
+//     });
+//   } else {
+//     https
+//       .createServer(
+//         {
+//           key: fs.readFileSync("./certificates/key.pem"),
+//           cert: fs.readFileSync("./certificates/cert.pem"),
+//           passphrase: process.env.PASSPHRASE
+//         },
+//         app
+//       )
+//       .listen(port, function() {
+//         console.log("Server is listening at http://%s:%s", port);
+//       });
+//   }
+// });
